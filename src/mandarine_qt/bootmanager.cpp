b23537f6ce4036688a2fa298bfd8eb20afebcd6b
ptyfyre <254117058+ptyfyre@users.noreply.github.com>
2026-04-08 10:32:44 +0200

    Suppress Qt deprecation warnings around image ops

--- src/mandarine_qt/bootmanager.cpp
+++ src/mandarine_qt/bootmanager.cpp
@@ -708,7 +708,12 @@ void GRenderWindow::CaptureScreenshot(u32 res_scale, const QString& screenshot_p
         screenshot_image.bits(),
         [this, screenshot_path](bool invert_y) {
             const std::string std_screenshot_path = screenshot_path.toStdString();
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
             if (screenshot_image.mirrored(false, invert_y).save(screenshot_path)) {
+QT_WARNING_POP
+
                 LOG_INFO(Frontend, "Screenshot saved to \"{}\"", std_screenshot_path);
             } else {
                 LOG_ERROR(Frontend, "Failed to save screenshot to \"{}\"", std_screenshot_path);
--- src/mandarine_qt/camera/camera_util.cpp
+++ src/mandarine_qt/camera/camera_util.cpp
@@ -213,9 +213,14 @@ std::vector<u16> ProcessImage(const QImage& image, int width, int height, bool o
     }
     QImage scaled =
         image.scaled(width, height, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
     QImage transformed =
         scaled.copy((scaled.width() - width) / 2, (scaled.height() - height) / 2, width, height)
             .mirrored(flip_horizontal, flip_vertical);
+QT_WARNING_POP
+
     if (output_rgb) {
         QImage converted = transformed.convertToFormat(QImage::Format_RGB16);
         std::memcpy(buffer.data(), converted.bits(), width * height * sizeof(u16));
